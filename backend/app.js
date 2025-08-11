1818f3165a23ced5151eb6e289cf99b9bfef1a55
itsshivamnith <thisisshivam.nith@gmail.com>
2025-08-11 21:08:26 +0530

    backend folder initialize

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+
+app.get('/about', (req, res) => {
+    res.send('Hello ');
+});
+
+
+
+module.exports = app;
